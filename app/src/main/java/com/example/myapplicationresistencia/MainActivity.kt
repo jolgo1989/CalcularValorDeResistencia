807b97f550bc5250b564928d010205524221de95
Jolgo <jlgo60@hotmail.com>
2020-09-09 05:12:08 -0500

    Se agrego rm -rf .git/refs/heads/

---
+++ app/src/main/java/com/example/myapplicationresistencia/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.myapplicationresistencia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val lista = arrayOf("1","2","3","4","5","6")
+        val adaptador1 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista)
+        spinnerbanda1.adapter = adaptador1
+
+        val lista2 = arrayOf("1","2","3","4","5","6")
+        val adaptador2 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista2)
+        spinnerbanda2.adapter = adaptador2
+
+        val lista3 = arrayOf("1","2","3","4","5","6")
+        val adaptador3 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista3)
+        spinnerbanda3.adapter = adaptador3
+
+        val lista4 = arrayOf("1","2","3","4","5","6")
+        val adaptador4 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista4)
+        spinnerbanda4.adapter = adaptador4
+
+
+        spinnerbanda4.onItemSelectedListener = object :
+
+
+            AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                textView.text = ("Hola mundo")
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+
+            }
+
+        }
+    }
+
+}
