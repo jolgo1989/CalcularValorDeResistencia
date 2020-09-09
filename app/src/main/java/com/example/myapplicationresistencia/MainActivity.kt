aa9010dc5ffb38cfe0550326fe8bc9658584de9a
Jolgo <jlgo60@hotmail.com>
2020-09-09 06:40:38 -0500

    Prueba para calcular el valor del resistencia

--- app/src/main/java/com/example/myapplicationresistencia/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationresistencia/MainActivity.kt
@@ -1,46 +1,89 @@
 package com.example.myapplicationresistencia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var acumulador = ""
+    private var termino1 = ""
+    private val termino2 = ""
+    private val termino4 = ""
+
+    var termino3 = 0.0
+    var resultado : Double = 0.0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
-        val lista = arrayOf("1","2","3","4","5","6")
-        val adaptador1 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista)
-        spinnerbanda1.adapter = adaptador1
+        val lista = arrayOf("1", "2", "3", "4", "5", "6")
+        val adaptador1 = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lista)
+        spinnerBanda1.adapter = adaptador1
 
-        val lista2 = arrayOf("1","2","3","4","5","6")
-        val adaptador2 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista2)
-        spinnerbanda4.adapter = adaptador2
+        val lista2 = arrayOf("1", "2", "3", "4", "5", "6")
+        val adaptador2 = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lista2)
+        spinnerBanda2.adapter = adaptador2
 
-        val lista3 = arrayOf("1","2","3","4","5","6")
-        val adaptador3 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista3)
-        spinnerbanda4.adapter = adaptador3
+        val lista3 = arrayOf("1", "2", "3", "4", "5", "6")
+        val adaptador3 = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lista3)
+        spinnerBanda3.adapter = adaptador3
 
-        val lista4 = arrayOf("1","2","3","4","5","6")
-        val adaptador4 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista4)
-        spinnerbanda3.adapter = adaptador4
+        val lista4 = arrayOf("1", "2", "3", "4", "5", "6")
+        val adaptador4 = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lista4)
+        spinnerMultiplicador.adapter = adaptador4
+
+        when (spinnerBanda1.selectedItem.toString()) {
+            "1" -> termino1 = "1"
+               // val nf: NumberFormat = NumberFormat.getInstance()
+               //val number: Double = nf.parse(myString).doubleValue()
+        }
+
+        acumulador = termino1 + termino2
+        resultado = acumulador.toDouble()
+        textView.text = java.lang.Double.toString(resultado) + "Ω" + termino4
+
+        when (spinnerBanda2.selectedItem.toString()) {
+            "2" -> termino1 = "2"
+        }
+        acumulador = termino1 + termino2
+        resultado = acumulador.toDouble()
+        textView.text = java.lang.Double.toString(resultado) + "Ω" + termino4
+
+        when (spinnerBanda3.selectedItem.toString()) {
+            "3" -> termino1 = "3"
+        }
+        acumulador = termino1 + termino2
+        resultado = acumulador.toDouble()
+        textView.text = java.lang.Double.toString(resultado) + "Ω" + termino4
+
+            spinnerMultiplicador.onItemSelectedListener = object :
+
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    if (termino1 != "" && termino2 != "") {
+                        acumulador = termino1 + termino2
+                        val resultado = acumulador.toDouble() * termino3
+                        textView.text = resultado.toString() + "Ω" + termino4
+                    }
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+
+                }
+
+            }
+
+
+
+
 
 //Metodo para mostrar el resultado al dar click en el spinnerMultiplicador
-        spinnerbanda3.onItemSelectedListener = object :
 
-            AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                textView.text = ("Hola a todos")
-            }
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-
-            }
-
-        }
     }
-
 }
