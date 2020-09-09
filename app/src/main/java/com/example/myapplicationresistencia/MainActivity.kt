8e08a2a16cd4dfd67f1a27c8e68871d807bb34b8
Jolgo <jlgo60@hotmail.com>
2020-09-09 05:33:32 -0500

    Se asigno nombre a los spinner

--- app/src/main/java/com/example/myapplicationresistencia/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationresistencia/MainActivity.kt
@@ -5,8 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,27 +19,24 @@ class MainActivity : AppCompatActivity() {
 
         val lista2 = arrayOf("1","2","3","4","5","6")
         val adaptador2 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista2)
-        spinnerbanda2.adapter = adaptador2
+        spinnerbanda4.adapter = adaptador2
 
         val lista3 = arrayOf("1","2","3","4","5","6")
         val adaptador3 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista3)
-        spinnerbanda3.adapter = adaptador3
+        spinnerbanda4.adapter = adaptador3
 
         val lista4 = arrayOf("1","2","3","4","5","6")
         val adaptador4 = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,lista4)
-        spinnerbanda4.adapter = adaptador4
+        spinnerbanda3.adapter = adaptador4
 
-
-        spinnerbanda4.onItemSelectedListener = object :
-
+//Metodo para mostrar el resultado al dar click en el spinnerMultiplicador
+        spinnerbanda3.onItemSelectedListener = object :
 
             AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 textView.text = ("Hola a todos")
             }
-
             override fun onNothingSelected(p0: AdapterView<*>?) {
-
 
             }
 
